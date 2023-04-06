ded250392844e91b1f75c97e52308c14c20ea6a9
Hyun-Tae Jin <htjmario@gmail.com>
2023-04-05 23:57:33 -0400

    Revert "extra info"
    This reverts commit 7110001965ecc36759184d60445b0d1c5641e0a8.

--- week4/day3/improve_codewars.py
+++
@@ -1,9 +0,0 @@
-# Problem 1 - Equal Sides Of An Array
-# https://www.codewars.com/kata/5679aa472b8f57fb8c000047
-
-def find_even_index(arr):
-    for index, num in enumerate(arr):
-        if sum(arr[:index]) == sum(arr[index+1:]):
-            return index
-    return -1
-
