77a95332b86405ecd85f9690fb487c7d2b666d23
Hyun-Tae Jin <htjmario@gmail.com>
2023-04-16 17:53:04 -0400

    redid ERD and pushing up create tables

--- week5/day3/movie_theater_create.sql
+++ week5/day3/movie_theater_create.sql
@@ -1,3 +1,23 @@
+CREATE TABLE employee (
+    employee_id SERIAL PRIMARY KEY,
+    fname VARCHAR(50),
+    lname VARCHAR(50),
+    phone VARCHAR(20),
+    email VARCHAR(80),
+    schedule DATE
+);
+
+CREATE TABLE concession (
+    concession_id SERIAL PRIMARY KEY,
+    product VARCHAR(30),
+    quantity INTEGER,
+    price INTEGER,
+    customer_id INTEGER,
+    employee_id INTEGER,
+    FOREIGN KEY (employee_id)
+        REFERENCES employee(employee_id)
+);
+
 CREATE TABLE customer (
     customer_id SERIAL PRIMARY KEY,
     fname VARCHAR(50),
@@ -16,37 +36,6 @@ CREATE TABLE movie (
     rating VARCHAR(20)
 );
 
-CREATE TABLE ticket (
-    ticket_id SERIAL PRIMARY KEY,
-    price INTEGER,
-    quantity INTEGER,
-    time DATE,
-    customer_id INTEGER,
-    movie_id INTEGER,
-    FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
-    FOREIGN KEY (movie_id) REFERENCES movie(movie_id)
-);
-
-CREATE TABLE employee (
-    employee_id SERIAL PRIMARY KEY,
-    fname VARCHAR(50),
-    lname VARCHAR(50),
-    phone VARCHAR(20),
-    email VARCHAR(80),
-    schedule VARCHAR(10)
-);
-
-CREATE TABLE concession (
-    concession_id SERIAL PRIMARY KEY,
-    product VARCHAR(30),
-    quantity INTEGER,
-    price INTEGER,
-    customer_id INTEGER,
-    employee_id INTEGER,
-    FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
-    FOREIGN KEY (employee_id) REFERENCES employee(employee_id)
-);
-
 CREATE TABLE room (
     room_id SERIAL PRIMARY KEY,
     capacity INTEGER,
@@ -54,6 +43,31 @@ CREATE TABLE room (
     doors INTEGER,
     movie_id INTEGER,
     employee_id INTEGER,
-    FOREIGN KEY (movie_id) REFERENCES movie(movie_id),
-    FOREIGN KEY (employee_id) REFERENCES employee(employee_id)
+    FOREIGN KEY (movie_id)
+        REFERENCES movie(movie_id),
+    FOREIGN KEY (employee_id)
+        REFERENCES employee(employee_id)
+);
+
+CREATE TABLE ticket (
+    ticket_id SERIAL PRIMARY KEY,
+    price INTEGER,
+    quantity INTEGER,
+    time DATE,
+    customer_id INTEGER,
+    movie_id INTEGER,
+    FOREIGN KEY (movie_id)
+        REFERENCES movie(movie_id),
+    FOREIGN KEY (customer_id)
+        REFERENCES customer(customer_id)
+);
+
+CREATE TABLE customer_concession (
+    sales_id SERIAL PRIMARY KEY,
+    customer_id INTEGER,
+    concession_id INTEGER,
+    FOREIGN KEY (customer_id)
+        REFERENCES customer(customer_id),
+    FOREIGN KEY (concession_id)
+        REFERENCES concession(concession_id)
 );
